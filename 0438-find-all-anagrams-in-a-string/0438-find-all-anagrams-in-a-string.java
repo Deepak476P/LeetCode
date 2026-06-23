84773e6462ddad42eb4a34a7475c9aabd3bbc2d5
Deepak476P <deepakreddy2k27@gmail.com>
2026-06-23 21:05:07 +0530

    Time: 50 ms (25.85%), Space: 47.7 MB (18.1%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,41 +1,36 @@
 class Solution {
 
     public List<Integer> findAnagrams(String s, String p) {
-
-        List<Integer> ans = new ArrayList<>();
-
-        if(s.length() < p.length()) {
-            return ans;
+        List<Integer> lis=new ArrayList<>();
+        HashMap<Character,Integer> m1=new HashMap<>();
+        HashMap<Character,Integer> m2=new HashMap<>();
+        if(p.length()>s.length()){
+            return lis;
         }
+        else{
+            for(char ch: p.toCharArray()){
+                m1.put(ch,m1.getOrDefault(ch,0)+1);
+            }
+            int low=0;
+            for(int i=0;i<s.length();i++){
+                char c=s.charAt(i);
+                m2.put(c, m2.getOrDefault(c, 0) + 1);
+                while((i-low+1)>p.length()){
+                    char rem=s.charAt(low);
+                    m2.put(rem, m2.get(rem) - 1);
+                    if(m2.get(rem)==0){
+                        m2.remove(rem);
+                    }
+                    
+                    low++;
+                }
+                if(m1.equals(m2)){
+                    lis.add(low);
+                }
 
-        int[] pFreq = new int[26];
-        int[] sFreq = new int[26];
-
-        // frequency of p
-        for(int i = 0; i < p.length(); i++) {
-            pFreq[p.charAt(i) - 'a']++;
-            sFreq[s.charAt(i) - 'a']++;
-        }
-
-        // first window
-        if(Arrays.equals(pFreq, sFreq)) {
-            ans.add(0);
-        }
-
-        // sliding window
-        for(int i = p.length(); i < s.length(); i++) {
-
-            // add new character
-            sFreq[s.charAt(i) - 'a']++;
-
-            // remove old character
-            sFreq[s.charAt(i - p.length()) - 'a']--;
-
-            if(Arrays.equals(pFreq, sFreq)) {
-                ans.add(i - p.length() + 1);
+                
             }
         }
-
-        return ans;
+        return lis;
     }
 }
